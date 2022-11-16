58decda537538232a6f3a41a504014d490662f93
Jin <novhunter@gmail.com>
2022-11-15 18:58:20 -0700

    added initial user input

---
+++ functions.py
@@ -0,0 +1,15 @@
+# making conversion calculator
+# By: Jin Starks
+
+# user input in length
+UserInput = input("What number to convert? ")
+print(UserInput)
+# choose starting unit
+
+# choose ending unit
+
+# conversion calculation
+
+# output of new length
+
+# display results
