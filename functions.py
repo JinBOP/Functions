58ad1e9f5b63af92a232ce6467b7ca217935483c
Jin <novhunter@gmail.com>
2022-11-22 17:36:46 -0700

    updated using template

--- functions.py
+++ functions.py
@@ -1,25 +1,39 @@
-# making conversion calculator
+# Conversion Calculator
 # By: Jin Starks
 
-# user input in length
-UserInput = float(input("What number to convert? "))
-print(UserInput)
-# choose starting unit
-InputUnit = input("In what Units? ")
-print(InputUnit)
-# choose ending unit
-# OutputUnit = input("Convert to what Units? ")
-# print(OutputUnit)
-# conversion calculation in to mm -> in * 25.4 and mm to in -> mm / 25.4
-# convert in to mm
-if(InputUnit == 'in'):
-    Output = UserInput * 25.4
-    OutputUnit = "in"
-elif(InputUnit == 'mm'):
-    Output = UserInput / 25.4
-    OutputUnit = "mm"
-else:
-    print("Not a valid Unit!")
-# display results
-print(Output, OutputUnit)
-# test comment
+# user input regarding the length to convert
+# get the unit from the user
+# convert the length to the correct unit
+# output the answer to the user
+def user_parser(user_input):
+# Do something
+# Separate the number from unit
+values = user_input.rsplit(" ")
+print (values)
+
+while True:
+user_input = input("number and unit to convert ")
+user_parser(user_input)
+# while True:
+# user_number = input("What number to convert? ")
+# if user_number.isdigit():
+# user_number = float(user_number)
+# break
+# else:
+# print ('please use a number')
+# user_unit = input("What unit is your number?")
+
+# if(user_unit == 'in'):
+# #perform in to mm
+# conv_number = user_number * 25.4
+# conv_unit = 'mm'
+# break
+# elif(user_unit == 'mm'):
+# #perform mm to in
+# conv_number = user_number / 25.4
+# conv_unit = 'in'
+# break
+# else:
+# print('That is not a valid unit')
+
+# print(conv_number, conv_unit)
